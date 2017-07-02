fc9e031bbb61f655510e81892ea2ef6d6cb87818
Yogesh  Srihari <yogeshgo05@gmail.com>
2017-07-01 17:40:11 -0700

    Hub and Chain saw first of the working register/event version
    Corner cases still need to resolved like duplicate hooks .
    register/channel events are smilar to rgister/events which is not
    implemented yet as they will channelId as filter .
    Will push complete version soon .

---
+++ README.md
@@ -0,0 +1,7 @@
+In order to run hub + chainsaw .
+Do the following . Soon and hub and chainsaw will be split in to different modules .
+#Step 1 :
+npm install
+
+#Step 2:
+npm start 
--- app/index.js
+++ app/index.js
@@ -3,17 +3,68 @@ const app = express() ;
 const http = require(`http`);
 const WebHooks = require('node-webhooks');
 const Web3 = require('web3');
-const config = require('../lib/configuration.js')
+const config = require('../lib/configuration.js');
+const chainsaw = require('../lib/chainsaw.js');
+const bodyParser = require(`body-parser`);
+const jsonfile = require('jsonfile');
+const webhookFile = './webhooks/data.json';
 
 const PORT = process.env.PORT || 6666;
 
 // Initialize web3 with testrpc provider
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
-var webHooks = new WebHooks({
-    db: './webHooksDB.json', // json file that store webhook URLs
-    httpSuccessCodes: [200, 201, 202, 203, 204], //optional success http status codes
-});
+const addHook = (contractEvent, contractHook) => {
+  jsonfile.readFile(webhookFile, function(err,obj){
+    if (err)
+    {
+      // Create new file and add the webhooks .
+      let webhook = {
+        [contractEvent]: [contractHook]
+      };
+      // Create and Add a file .
+      jsonfile.writeFile(webhookFile, webhook, function(err){
+        console.log(err);
+      });
+    }
+    else{
+      // TODO : Don't append duplicate hooks .
+      // Appending to the incoming object .
+      obj[contractEvent].push(contractHook);
+
+      // write to file now
+      jsonfile.writeFile(webhookFile, obj, function(err){
+        console.log(err);
+      });
+    }
+  });
+}
+
+const deleteHook = (contractEvent, contractHook) => {
+  // Todo : Delete an webhook, will implement
+  // this soon .
+}
+
+const findEventAndWatch = (contractEvent, contractHook) => {
+  switch(contractEvent){
+    case 'DidDeposit':
+      chainsaw.didDepositEvent() ;
+      break ;
+    case 'DidSettle':
+      chainsaw.didSettleEvent() ;
+      break ;
+    case 'DidStartSettle':
+      chainsaw.didStartSettleEvent() ;
+      break ;
+    default:
+      break ;
+  }
+}
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get("/", (req, res) => {
   res.setHeader(`Cache-Control`, `public, max-age=86400`);
@@ -22,35 +73,21 @@ app.get("/", (req, res) => {
   res.end("Hello from Hub");
 });
 
+// TODO : remove this when you have actual hooks.
+app.post("/dummy/hook", (req, res) => {
+  console.log("Webhook called!");
+  res.end(`{'Ok' : 200 }`);
+});
+
 app.post("/register/event", (req, res) => {
   const contractEvent = req.body.event;
-  const contractAddress = req.body.contractAddress;
   const contractHook = req.body.hook ;
 
-  // Since it web hook is a key value pair .
-  const origin = req.headers.origin ;
-  console.log("contractEvent , contractAddress , contractHook");
-  console.log(contractEvent);
-  console.log(contractAddress);
-  console.log(contractHook);
-  // May be we should do origin check here
-  // if (origin != "xxxx.spankchain.com:pppp")
-  // {
-  //
-  // }
+  // Adding the data to hook .
+  addHook(contractEvent, contractHook);
 
-  // Each key is unique . An app which wants notification
-  // can only listen to one hook per event and per contract .
-  const uniqueKey = contractEvent + ":"+ contractAddress + ":"
-                    origin ;
-
-  // Add the webhook
-  webhooks.add(uniqueKey, contractHook).then(function(){
-      console.log("Webhook added .");
-  }).catch(function(err){
-
-  }) ;
-
+  findEventAndWatch(contractEvent,contractHook); 
+  res.end(`{'Ok' : 200 }`);
 });
 
 app.delete("/register/event", (req, res) => {
@@ -58,6 +95,7 @@ app.delete("/register/event", (req, res) => {
   const contractAddress = req.body.contractAddress;
   const contractHook = req.body.hook ;
 
+  res.end(`{'Ok' : 200 }`);
 });
 
 app.post("/register/channel", (req, res) => {
@@ -71,7 +109,6 @@ app.delete("/register/channel", (req, res) => {
   const contractHook = req.body.hook ;
 
 });
-
 
 http.createServer(app).listen(PORT, (err) => {
   if (err) throw err;
